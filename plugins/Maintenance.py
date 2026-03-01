fa92b621ba1c94d32c93b6ed409e179ccbb9509c
ak4422920 <ak4422920@gmail.com>
2026-03-01 08:29:25 +0530

    Update support group mention in maintenance message

--- plugins/Maintenance.py
+++ plugins/Maintenance.py
@@ -11,7 +11,7 @@ async def maintenance_checker(client, message: Message):
     chat_id = message.chat.id
     if MAINTENANCE_MODE and user_id not in BYPASS_IDS and chat_id not in BYPASS_IDS:
         await message.reply(
-            "🚧 **Bot is under Maintenance!**\n\nPlease try again later.\n\nSupport Group: @RexBots_Official",
+            "🚧 **Bot is under Maintenance!**\n\nPlease try again later.\n\nSupport Group: @AkMovieVerse",
             quote=True
         )
         return
